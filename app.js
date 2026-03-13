bb7d6c1b1eb350798c93359d019ce2929eb80f69
HoangPhi_CSE <phphi.cse@gmail.com>
2026-03-14 03:39:06 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const contactsRoutes = require('./app/routes/contact.route');
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application.' });
 });
+
+app.use('/api/contacts', contactsRoutes);
 
 module.exports = app;
